87dc8058f61e1be052aa1be070bb159561dc0a5a
Sotun-1010 <oluwasotunogunjirin@gmail.com>
2025-09-09 23:19:22 +0100

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,30 @@
+# Dash App for customer segmentation analysis
+<!-- TABLE OF CONTENTS -->
+
+## Table of Contents
+
+- [Overview](#overview)
+- [Tools](#tools)
+- [Contact](#contact)
+
+<!-- OVERVIEW -->
+
+## Overview
+
+![screenshot](image.png)
+
+In this project, I built a python dash app for customer segmentation analysis from my previous customer segmentation project. The app uses K-Means clustering to segment customers based on their behavior and preferences. This app allows users see the interactions of various features within the customer segments available. The app also allows users to explore the customer segments based on various features.
+
+## Tools
+
+- [Pandas](https://pandas.pydata.org)
+- [Plotly](https://plotly.com/python/?_gl=1*1ki74vb*_gcl_au*OTI4MzA2MTkwLjE3NTc0NTQ0MTY.*_ga*NTg3OTQ0NTE3LjE3NTc0NTQ0MTU.*_ga_6G7EE0JNSC*czE3NTc0NTQ0MTYkbzEkZzEkdDE3NTc0NTQ0NDIkajM0JGwwJGgw)
+- [Dash](https://dash.plotly.com/?_gl=1*urssjf*_gcl_au*OTI4MzA2MTkwLjE3NTc0NTQ0MTY.*_ga*NTg3OTQ0NTE3LjE3NTc0NTQ0MTU.*_ga_6G7EE0JNSC*czE3NTc0NTQ0MTYkbzEkZzEkdDE3NTc0NTQ1MDEkajM1JGwwJGgw)
+
+## Contact
+
+- GitHub [@Sotun-1010](https://github.com/Sotun-1010)
+- Twitter [@O_G_Sotun](https://twitter.com/O_G_Sotun?t=kRiO1YNhYKn8NJJnxTZ42A&s=03)
+- Portfolio Site [Ogunjirin Oluwasotun](https://www.datascienceportfol.io/oluwasotunogunjirind)
+- Medium [Ogunjirin Oluwasotun Goodness](https://medium.com/@oluwasotunogunjirin)
+- Website [Oglytics](https://oglytics.webflow.io/)
---
+++ app.py
@@ -0,0 +1,73 @@
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv('df_filtered.csv')
+
+
+columns = df.columns.drop(["Unnamed: 0", "Cluster"]).tolist()
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    html.H1('3D Scatter plot of Customer Data from cluster analysis'),
+    
+    html.Div([
+        # Dropdown for the X-axis
+        html.Label("X-axis:"),
+        dcc.Dropdown(
+            id='x-axis-dropdown',
+            options=[{'label': i, 'value': i} for i in columns],
+            value="Age",  
+            clearable=False
+        ),
+        # Dropdown for the Y-axis
+        html.Label("Y-axis:"),
+        dcc.Dropdown(
+            id='y-axis-dropdown',
+            options=[{'label': i, 'value': i} for i in columns],
+            value="Income",
+            clearable=False
+        ),
+        # Dropdown for the Z-axis
+        html.Label("Z-axis:"),
+        dcc.Dropdown(
+            id='z-axis-dropdown',
+            options=[{'label': i, 'value': i} for i in columns],
+            value="Sex",
+            clearable=False
+        )
+    ], style={'width': '50%', 'display': 'inline-block'}),
+
+    # 3D scatter plot graph component
+    dcc.Graph(id='3d-scatter-plot')
+])
+
+# 4. Create a Callback to Update the Plot
+# This function is triggered whenever the value of any dropdown changes
+@app.callback(
+    Output('3d-scatter-plot', 'figure'),
+    [Input('x-axis-dropdown', 'value'),
+     Input('y-axis-dropdown', 'value'),
+     Input('z-axis-dropdown', 'value')]
+)
+
+def update_graph(x_col, y_col, z_col):
+    """
+    Updates the 3D scatter plot based on the selected columns.
+    """
+    fig = px.scatter_3d(
+        df,
+        x=x_col,
+        y=y_col,
+        z=z_col,
+        color='Cluster',
+        title=f'3D Scatter Plot of {x_col} vs {y_col} vs {z_col}'
+    )
+    return fig
+
+# 5. Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
